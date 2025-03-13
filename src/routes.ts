75dd0fd576baaba9873534a99e9b4420be0f4cb1
Daniel <danieldsm.on@gmail.com>
2025-03-13 15:44:28 -0300

    feat: create routes

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import type { Request, Response } from 'express'
+
+const router = Router()
+
+router.get('/', (req: Request, res: Response) => {
+	res.send('Hello World!')
+})
+
+export { router }
