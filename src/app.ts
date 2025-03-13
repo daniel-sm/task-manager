6ffc2584f6113762666203caabc8fc8c1b058847
Daniel <danieldsm.on@gmail.com>
2025-03-13 15:37:12 -0300

    style: format app file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import type { Request, Response } from 'express';
+import express from 'express'
+import type { Request, Response } from 'express'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
-});
+	res.send('Hello, world!')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+	console.log(`Server is running on http://localhost:${port}`)
+})
