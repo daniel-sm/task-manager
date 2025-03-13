2f9bb8afd7dd86208026acd22e385dddab69a890
Daniel <danieldsm.on@gmail.com>
2025-03-13 15:29:43 -0300

    feat: create server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
