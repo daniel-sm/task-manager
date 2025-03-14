33424bfe44c50d2ed5bcdf201a0e12a4f2f62ed6
Daniel <danieldsm.on@gmail.com>
2025-03-14 15:41:08 -0300

    wip: create register route
    - add zod validation to register route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { router } from './routes'
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.use('/api', router)
 
 app.listen(port, () => {
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import type { Request, Response } from 'express'
+import { bodyValidator } from './middlewares'
+import { registerSchema } from './schemas'
 
 const router = Router()
 
-router.post('/auth/register', (req: Request, res: Response) => {
-	res.send('register')
-})
+router.post(
+	'/auth/register',
+	bodyValidator(registerSchema),
+	(req: Request, res: Response) => {
+		res.send('register')
+	},
+)
 
 export { router }
