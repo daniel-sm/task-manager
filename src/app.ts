d1355078e77b01259f2f9cb861ed4f1d5e4e47f7
Daniel <danieldsm.on@gmail.com>
2025-03-17 16:50:14 -0300

    refactor: update running server message

--- src/app.ts
+++ src/app.ts
@@ -9,5 +9,5 @@ app.use(express.json())
 app.use('/api', router)
 
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`)
+	console.log(`Server is running on port ${port}`)
 })
