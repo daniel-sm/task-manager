abdd6b440252dc98313fa952d507e74e64039bc4
Daniel <danieldsm.on@gmail.com>
2025-03-26 14:42:42 -0300

    chore: add database url to env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 const envSchema = z.object({
 	PORT: z.coerce.number().default(3000),
 	SECRET_KEY: z.string(),
+	DATABASE_URL: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
