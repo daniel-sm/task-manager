b83521fb39afc58a1835d6754958fea106e058fc
Daniel <danieldsm.on@gmail.com>
2025-04-08 17:16:47 -0300

    feat: add user routes with auth middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { env } from './config/env'
 import { authMiddleware } from './middlewares/auth'
 import { authRouter } from './routes/auth-routes'
 import { tasksRouter } from './routes/task-routes'
+import { userRouter } from './routes/user-routes'
 
 const app = express()
 const port = env.PORT
@@ -11,6 +12,7 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/tasks', authMiddleware, tasksRouter)
+app.use('/api/users', authMiddleware, userRouter)
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
